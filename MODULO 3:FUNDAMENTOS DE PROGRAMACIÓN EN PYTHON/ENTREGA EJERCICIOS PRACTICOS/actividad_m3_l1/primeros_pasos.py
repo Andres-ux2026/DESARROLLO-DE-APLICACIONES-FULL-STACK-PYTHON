ee59894b3bc77fd1c5eb8a58da7bf0c0093bd58e
Andres-ux2026 <moraga_2006@hotmail.com>
2026-03-11 12:53:43 +0000

    entrega ejercicio leccion 1 python

---
+++ MODULO 3:FUNDAMENTOS DE PROGRAMACIÓN EN PYTHON/ENTREGA EJERCICIOS PRACTICOS/actividad_m3_l1/primeros_pasos.py
@@ -0,0 +1,20 @@
+import math
+
+
+print("¡Hola! Este es mi primer programa en Python.")
+
+nombre= "Andres"
+edad="18"
+ciudad="tal-tal"
+
+print((nombre)+" "+" "+(edad)+" "+" "+(ciudad))
+
+
+numero1=10  #declaro variable 1
+numero2=20 #declaro variable 2
+suma=(numero1+numero2) # le asigno a la variable suma la operacion matematica
+print(suma) # imprimo la variable suma
+
+
+raiz=math.sqrt(9)
+print(raiz)
